e250dfecd43f2496ce66608e79c24f99c75767bb
bojko108 <bojko108@gmail.com>
2022-07-25 11:52:05 +0300

    update livescore ext to v3

--- livescore-extension/contentScript.js
+++ livescore-extension/contentScript.js
@@ -1,65 +1,53 @@
-console.log('Livescore Extension is now available!');
+console.log(`Livescore Extension is now available!\nHighlights Arsenal FC in the page!`);
+
+const regex = /^["']?arsenal["']?$/gim;
 
 /**
  * Finds an HTMLElement cointaning text equal to 'arsenal' in an HTMLElementCollection
  * @param {HTMLElementCollection} [container]
  * @return {HTMLElement} element
  */
-findArsenal = container => {
-  let result;
-  if (container && container.length > 0) {
-    for (let i = 0; i < container.length; i++) {
-      const item = container.item(i);
-      if (item && item.innerText.toLowerCase() === 'arsenal') {
-        result = item;
-        break;
-      }
+findArsenal = (container) => {
+    let result;
+    if (container && container.length > 0) {
+        for (let i = 0; i < container.length; i++) {
+            const item = container.item(i);
+            if (item && item.innerText.toLowerCase() === 'arsenal') {
+                result = item;
+                break;
+            }
+        }
     }
-  }
-  return result;
+    return result;
 };
 
 /**
  * Highlights an HTMLElement. Changes its background color to Arsenal red, text color to white and makes the font bolder.
  * @param {HTMLElement} element
  */
-highlightElement = element => {
-  element.style.backgroundColor = '#f00000';
-  element.style.color = 'white';
-  element.style.fontWeight = 'bold';
-};
-
-/**
- * Searches for any <span> element containing 'arsenal' and highlights it
- */
-highlightMatches = () => {
-  const spans = document.getElementsByTagName('span');
-  const item = findArsenal(spans);
-  if (item) {
-    highlightElement(item.parentElement.parentElement);
-  }
+highlightElement = (element) => {
+    element.style.fontWeight = 'bold'
+    element.parentElement.parentElement.style.color = 'white'
+    element.parentElement.parentElement.style.backgroundColor = '#f00000'
 };
 
 /**
  * Highlights FC Arsenal's current position in league table
  */
-highlightLeaguePosition = () => {
-  const tables = document.getElementsByClassName('league-table');
+highlightElements = () => {
+    const divs = document.querySelectorAll('div,a')
 
-  if (tables && tables.length > 0) {
-    for (let i = 0; i < tables.length; i++) {
-      const table = tables.item(i);
-      if (table) {
-        const teams = table.getElementsByClassName('team');
-        const team = findArsenal(teams);
-        if (team) {
-          highlightElement(team.parentElement);
+    if (divs && divs.length > 0) {
+        for (let i = 0; i < divs.length; i++) {
+            const item = divs.item(i);
+            if (item) {
+                if (regex.test(item.textContent)) {
+                    highlightElement(item);
+                }
+            }
         }
-      }
     }
-  }
 };
 
 // run above functions and highlight FC Arsenal in the page
-highlightMatches();
-highlightLeaguePosition();
+highlightElements();
