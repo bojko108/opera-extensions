682137afba22190087192165692eabdb05810031
bojko108 <bojko108@gmail.com>
2022-07-25 11:51:50 +0300

    update urls for cadastre ext

--- pontech-extension-cadastre/external-script.js
+++ pontech-extension-cadastre/external-script.js
@@ -3,9 +3,9 @@
  * values will be added to the web page control.
  */
 const slks = [
-  { value: null, label: 'Отвори ЧЕЗ ГИС Клиент...' },
-  { value: 'https://gis.cezbg.corp/ArcFMWebViewer/index.html?viewer=cez', label: 'ЧЕЗ Разпределение България' },
-  { value: 'https://gis.cezbg.corp/ArcFMWebViewer/index.html?viewer=survey', label: 'Бъдещи присъединявания' },
+    { value: null, label: 'Отвори ЕРМ Запад ГИС...' },
+    { value: 'https://gis.cezbg.corp/ArcFMWebViewer/index.html?viewer=main', label: 'ЕРМ Запад ГИС' },
+    { value: 'https://gis.cezbg.corp/ArcFMWebViewer/index.html?viewer=survey', label: 'Бъдещи присъединявания' },
 ];
 
 /**
@@ -16,22 +16,22 @@ const slks = [
  * @returns {Array<Number>} map's center coordinates in form <x,y>
  */
 const getCoordinates = (targetEpsgCode = 'EPSG:4326', precision = 6) => {
-  const mapView = window.mainMap.OlMapInstance.getView();
-  const mapEpsgCode = mapView.getProjection().getCode();
-  const mapCenter = mapView.getCenter();
-  const coordinates = window.ol.proj.transform(mapCenter, mapEpsgCode, targetEpsgCode);
-  return coordinates.map((c) => c.toFixed(precision));
+    const mapView = window.mainMap.OlMapInstance.getView();
+    const mapEpsgCode = mapView.getProjection().getCode();
+    const mapCenter = mapView.getCenter();
+    const coordinates = window.ol.proj.transform(mapCenter, mapEpsgCode, targetEpsgCode);
+    return coordinates.map((c) => c.toFixed(precision));
 };
 /**
  * Gets current map scale
  * @returns {Number} map scale
  */
 const getScale = () => {
-  const view = window.mainMap.OlMapInstance.getView();
-  const projection = view.getProjection();
-  const mpu = ol.proj.Units.METERS_PER_UNIT[projection.getUnits()];
-  const scale = view.getResolution() * mpu * 39.37 * (25.4 / 0.28);
-  return Math.round(scale);
+    const view = window.mainMap.OlMapInstance.getView();
+    const projection = view.getProjection();
+    const mpu = ol.proj.Units.METERS_PER_UNIT[projection.getUnits()];
+    const scale = view.getResolution() * mpu * 39.37 * (25.4 / 0.28);
+    return Math.round(scale);
 };
 /**
  * Format input URL address by adding map's center and scale values
@@ -39,73 +39,73 @@ const getScale = () => {
  * @returns {String} provided URL with added parameters
  */
 const formatUrlForSLK = (url) => {
-  // by default the coordinates will be send in Geographic coordinates (EPSG:4326)
-  const coordinates = getCoordinates();
-  const scale = getScale();
-  return `${url}${url.indexOf('?') > -1 ? '&' : '?'}center=${coordinates[0]},${coordinates[1]},4326&scale=${scale}`;
+    // by default the coordinates will be send in Geographic coordinates (EPSG:4326)
+    const coordinates = getCoordinates();
+    const scale = getScale();
+    return `${url}${url.indexOf('?') > -1 ? '&' : '?'}center=${coordinates[0]},${coordinates[1]},4326&scale=${scale}`;
 };
 
 const createButton = (text) => {
-  let button = document.createElement('button');
-  button.appendChild(document.createTextNode(text));
-  button.style.width = '100px';
-  button.style.height = '30px';
-  button.style.font = '12px Roboto,Ubuntu';
-  button.style.width = '100px';
-  button.style.height = '30px';
-  button.style.border = '1px solid white';
-  button.style.backgroundColor = 'green';
-  button.style.color = 'white';
+    let button = document.createElement('button');
+    button.appendChild(document.createTextNode(text));
+    button.style.width = '100px';
+    button.style.height = '30px';
+    button.style.font = '12px Roboto,Ubuntu';
+    button.style.width = '100px';
+    button.style.height = '30px';
+    button.style.border = '1px solid white';
+    button.style.backgroundColor = 'green';
+    button.style.color = 'white';
 
-  return button;
+    return button;
 };
 
-window.onload = function () {
-  const container = document.querySelector('div.map-control.map-control-inner');
-  if (container) {
-    const item = document.createElement('span');
-    item.className = 'mc-controlbox';
-    item.style.display = 'inline-flex';
+window.onload = function() {
+    const container = document.querySelector('div.map-control.map-control-inner');
+    if (container) {
+        const item = document.createElement('span');
+        item.className = 'mc-controlbox';
+        item.style.display = 'inline-flex';
 
-    const buttonCopyCoordinates = createButton('Копирай координатите');
-    buttonCopyCoordinates.onclick = () => {
-      const coordinates = getCoordinates('EPSG:25835', 0); // UTM35N for ETRS89
-      const text = `${coordinates[0]}, ${coordinates[1]}`;
-      navigator.clipboard.writeText(text).then(
-        () => {
-          alert(`Координати ${text} (UTM 35N) бяха копирани в клипборда!`);
-        },
-        (err) => {
-          alert('Грешка: ', err);
+        const buttonCopyCoordinates = createButton('Копирай координатите');
+        buttonCopyCoordinates.onclick = () => {
+            const coordinates = getCoordinates('EPSG:25835', 0); // UTM35N for ETRS89
+            const text = `${coordinates[0]}, ${coordinates[1]}`;
+            navigator.clipboard.writeText(text).then(
+                () => {
+                    alert(`Координати ${text} (UTM 35N) бяха копирани в клипборда!`);
+                },
+                (err) => {
+                    alert('Грешка: ', err);
+                }
+            );
+        };
+
+        const selectOpenSlk = document.createElement('select');
+        selectOpenSlk.id = 'select-slk';
+        selectOpenSlk.style.width = '200px';
+        selectOpenSlk.onchange = ({ target }) => {
+            if (!target) {
+                return;
+            }
+            if (target.value) {
+                const url = formatUrlForSLK(target.value);
+                console.log(url);
+                window.open(url, '_blank');
+                target.value = null;
+            }
+        };
+
+        for (let i = 0; i < slks.length; i++) {
+            const option = document.createElement('option');
+            option.text = slks[i].label;
+            option.value = slks[i].value;
+            selectOpenSlk.appendChild(option);
         }
-      );
-    };
 
-    const selectOpenSlk = document.createElement('select');
-    selectOpenSlk.id = 'select-slk';
-    selectOpenSlk.style.width = '200px';
-    selectOpenSlk.onchange = ({ target }) => {
-      if (!target) {
-        return;
-      }
-      if (target.value) {
-        const url = formatUrlForSLK(target.value);
-        console.log(url);
-        window.open(url, '_blank');
-        target.value = null;
-      }
-    };
+        item.appendChild(buttonCopyCoordinates);
+        item.appendChild(selectOpenSlk);
 
-    for (let i = 0; i < slks.length; i++) {
-      const option = document.createElement('option');
-      option.text = slks[i].label;
-      option.value = slks[i].value;
-      selectOpenSlk.appendChild(option);
+        container.appendChild(item);
     }
-
-    item.appendChild(buttonCopyCoordinates);
-    item.appendChild(selectOpenSlk);
-
-    container.appendChild(item);
-  }
 };
